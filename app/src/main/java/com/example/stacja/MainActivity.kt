688f5684800277652613b5f11d53bb479f6b8b3d
lwroblew99 <lucja.wroblewska999@gmail.com>
2021-12-12 15:37:33 +0100

    lokalizacja

--- app/src/main/java/com/example/stacja/MainActivity.kt
+++ app/src/main/java/com/example/stacja/MainActivity.kt
@@ -2,6 +2,10 @@ package com.example.stacja
 
 import android.app.Activity
 import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Geocoder
+import android.location.Location
+import android.location.LocationManager
 import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -11,8 +15,12 @@ import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.InputMethodManager
 import android.widget.*
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
 import androidx.viewbinding.ViewBinding
 import com.example.stacja.databinding.ActivityMainBinding
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
@@ -26,6 +34,7 @@ import java.text.SimpleDateFormat
 import java.time.LocalDate
 import java.time.LocalTime
 import java.util.*
+import java.util.jar.Manifest
 
 //https://home.openweathermap.org/api_keys
 class MainActivity : AppCompatActivity() {
@@ -37,12 +46,15 @@ class MainActivity : AppCompatActivity() {
     private lateinit var tvCisnienie: TextView
     private lateinit var tvDate: TextView
     private lateinit var tvTime: TextView
-    var CITY: String = "Sopot"
+    lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+
+    var city: String = "Poznan"
     val API: String = "538641b64c380fbc31725377e486d0c1"
     val useruid: String ="X3hgaV4OrMfYSMh0HjkWEkncAN13"
     val webApi:String = "AIzaSyBUNk2SUBSJx78jNwUUKikIUP8udhFBYj0"
     val localDateNow = LocalDate.now()
     val localTimeNow = LocalTime.now()
+    var PERMISSION_ID = 1000
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -79,8 +91,57 @@ class MainActivity : AppCompatActivity() {
             }
         })
 
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+
+        checkLocationPermission()
+
         weather().execute()
 
+    }
+
+    private fun getLastLocation(){
+        if(checkLocationPermission()){
+            if(isLocationEnabled()){
+                fusedLocationProviderClient.lastLocation.addOnCompleteListener{task ->
+                    var location:Location? = task.result
+                    if(location == null){
+                        getLastLocation()
+                    }
+                }
+            }
+        }
+    }
+    private fun checkLocationPermission():Boolean {
+       // val task = fusedLocationProviderClient.lastLocation
+
+        if(ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED ||
+            ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED
+        )
+        {
+           return true
+
+        }
+        return false
+    }
+    fun RequestPermission(){
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(android.Manifest.permission.ACCESS_COARSE_LOCATION,android.Manifest.permission.ACCESS_FINE_LOCATION),
+            PERMISSION_ID
+        )
+    }
+
+    private fun isLocationEnabled():Boolean{
+        var locationManager = getSystemService(Context.LOCATION_SERVICE)as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
+    }
+
+    private fun getCityName(lat:Double,long:Double):String{
+        var CITY = ""
+        var geoCoder = Geocoder(this, Locale.getDefault())
+        var Adres = geoCoder.getFromLocation(lat,long,1)
+        CITY = Adres.get(0).locality
+        return CITY
     }
 
     inner class weather() : AsyncTask<String, Void, String>() {
@@ -95,7 +156,7 @@ class MainActivity : AppCompatActivity() {
             var response: String?
             try {
                 response =
-                    URL("https://api.openweathermap.org/data/2.5/weather?q=$CITY&units=metric&appid=$API")
+                    URL("https://api.openweathermap.org/data/2.5/weather?q=$city&units=metric&appid=$API")
                         .readText(Charsets.UTF_8)
             } catch (e: Exception) {
                 response = null
